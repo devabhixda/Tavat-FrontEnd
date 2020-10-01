363d44e439a973ed5ceea7d72481b91ac53194f4
Abhi Jain <devabhixda@gmail.com>
2020-10-01 20:19:26 +0530

    Initialize Connect app

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:connect/Screens/Onboarding/login.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Connect());
+}
+class Connect extends StatelessWidget {
+    @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      
+    );
+  }
+}
