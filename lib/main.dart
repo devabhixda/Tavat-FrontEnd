e39835262b07bbd73aadba42777fb0bcf3df3515
Abhi Jain <devabhixda@gmail.com>
2020-11-19 19:41:00 +0530

    Rename to Tavat

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,47 @@
-import 'package:connect/Screens/Onboarding/login.dart';
+import 'package:connect/Screens/Onboarding/phone.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(Connect());
-  Firebase.initializeApp();
+void main() async {
+  runApp(Main());
 }
-class Connect extends StatelessWidget {
-    @override
+
+class Main extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: login(),
+      home: Tavat()
+    );
+  }
+}
+
+class Tavat extends StatefulWidget {
+  @override
+  _TavatState createState() => _TavatState();
+}
+
+class _TavatState extends State<Tavat> {
+
+  bool loading = true;
+
+  void initState() {
+    super.initState();
+    Firebase.initializeApp().whenComplete(() { 
+      setState(() {
+        loading = false;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: loading ? Center(
+          child: CircularProgressIndicator()
+        ) : phone() ,
+      ),
     );
   }
 }
