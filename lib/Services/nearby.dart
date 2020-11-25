6abd3b92bff7c7603957e2e6d0e4b66b2dd6a92a
Abhi Jain <devabhixda@gmail.com>
2020-11-25 20:53:58 +0530

    Specify place type

--- lib/Services/nearby.dart
+++ lib/Services/nearby.dart
@@ -5,11 +5,12 @@ import 'dart:async';
 import 'dart:convert';
 
 Future<List<PlaceDetail>> getNearbyPlaces(double lat, double lng, double radius) async {
-  String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+lat.toString()+","+lng.toString()+"&radius="+radius.toString()+"&key="+key;
-  print(url);
-  var reponse = await http.get(url, headers: {"Accept": "application/json"});
-  List data = json.decode(reponse.body)["results"];
-  List<PlaceDetail> places;
-  data.forEach((f) => places.add(new PlaceDetail(f["place_id"], f["name"], f["icon"], f["rating"].toString())));
+  List<PlaceDetail> places = [];
+  for(String type in types) {
+    String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+lat.toString()+","+lng.toString()+"&type="+type+"&radius="+radius.toString()+"&key="+key;
+    var reponse = await http.get(url, headers: {"Accept": "application/json"});
+    List data = json.decode(reponse.body)["results"];
+    data.forEach((f) => places.add(new PlaceDetail(f["place_id"], f["name"], f["icon"], f["rating"].toString(), f["geometry"]["location"]["lat"], f["geometry"]["location"]["lng"])));
+  }
   return places;
 }
--- lib/consts.dart
+++ lib/consts.dart
@@ -3,3 +3,4 @@ import 'package:flutter/painting.dart';
 Color cred = Color(0xFFF61B39);
 Color bgrey = Color(0xFFF1F1F1);
 final key  = "AIzaSyAGQbtyuYYTBbokGTwA-qxPeCwIRuNBgjg";
+List types = ["restaurant", "cafe", "bar"];
