3a78ad6e528a7de25a2d0e61fd68c93d0ce65f1e
Abhi Jain <devabhixda@gmail.com>
2020-11-25 20:52:23 +0530

    Update Maps API key

--- lib/consts.dart
+++ lib/consts.dart
@@ -2,4 +2,4 @@ import 'package:flutter/painting.dart';
 
 Color cred = Color(0xFFF61B39);
 Color bgrey = Color(0xFFF1F1F1);
-final key  = "AIzaSyASWFW2ZVbUxel2J2CNNyE-_U85SBOQ0dw";
+final key  = "AIzaSyAGQbtyuYYTBbokGTwA-qxPeCwIRuNBgjg";
