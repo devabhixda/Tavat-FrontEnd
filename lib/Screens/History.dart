0f1c66af7a0c8b456089ac0c13cd176357ea3321
Abhi Jain <devabhixda@gmail.com>
2020-12-09 13:39:43 +0530

    Populate history page
    Also replace png with svg

--- lib/Screens/History.dart
+++ lib/Screens/History.dart
@@ -1,12 +1,39 @@
+import 'package:connect/Services/auth.dart';
 import 'package:connect/consts.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class History extends StatefulWidget {
   @override
   _HistoryState createState() => _HistoryState();
 }
 class _HistoryState extends State<History> {
+
+  Stream history;
+  String uid;
+  Auth auth = new Auth();
+
+  @override
+  void initState() {
+    getUserInfogetChats();
+    super.initState();
+  }
+
+  getUserInfogetChats() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String me = prefs.getString('uid');
+    setState(() {
+      uid = me;
+    });
+    auth.getHistory(uid).then((snapshots) {
+      setState(() {
+        history = snapshots;
+      });
+    });
+  }
+
   double h,w;
   @override    
   Widget build(BuildContext context) {    
@@ -26,50 +53,58 @@ class _HistoryState extends State<History> {
       body: Column(
         children: [
           Expanded(
-            child: ListView.builder(
-              padding: EdgeInsets.only(top: 0.02 * h),
-              itemCount: 15,
-              itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  margin: EdgeInsets.symmetric(horizontal: 0.05 * w, vertical: 10),
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                  elevation: 4,
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 0.05 * w, vertical: 10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+            child: StreamBuilder(
+              stream: history,
+              builder: (context, snapshot){
+                return snapshot.hasData ?  ListView.builder(
+                  itemCount: snapshot.data.documents.length,
+                  itemBuilder: (context, index){
+                    return Card(
+                      margin: EdgeInsets.symmetric(horizontal: 0.05 * w, vertical: 10),
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                      elevation: 4,
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 0.05 * w, vertical: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Text("Club Palooza",
-                              style: GoogleFonts.ptSans(
-                                fontSize: 22
-                              ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(snapshot.data.documents[index]["location"],
+                                  style: GoogleFonts.ptSans(
+                                    fontSize: 22
+                                  ),
+                                ),
+                                Container(
+                                  width: 0.6 * w,
+                                  child: Text(snapshot.data.documents[index]["vincinity"],
+                                    style: GoogleFonts.ptSans(
+                                      fontSize: 18
+                                    ),
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+                                Text("Visited on : "+snapshot.data.documents[index]["date"],
+                                  style: GoogleFonts.ptSans(
+                                    fontSize: 16
+                                  ),
+                                )
+                              ],
                             ),
-                            Text("111, MG road, Bangalore, India",
-                              style: GoogleFonts.ptSans(
-                                fontSize: 18
-                              ),
-                            ),
-                            Text("Visited on : 11/11/20",
-                              style: GoogleFonts.ptSans(
-                                fontSize: 16
-                              ),
+                            CircleAvatar(
+                              backgroundColor: cred,
+                              radius: 0.03 * h,
+                              child: SvgPicture.asset('assets/images/pin.svg')
                             )
                           ],
-                        ),
-                        CircleAvatar(
-                          backgroundColor: cred,
-                          radius: 0.03 * h,
-                          child: Image.asset('assets/images/pin.png')
                         )
-                      ],
-                    )
-                  )
-                );
-              }
-            ),
+                      )
+                    );
+                  }
+                ) : Container();
+              },
+            )
           )
         ],
       )
